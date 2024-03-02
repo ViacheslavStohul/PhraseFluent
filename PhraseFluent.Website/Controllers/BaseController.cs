0a7a6613677da842ceb039c86b025ef7738df6f1
Viacheslav Stohul <slvstgl90238@gmail.com>
2024-03-02 20:19:50 +0200

    Add UserId property to BaseController
    The UserId property was added to the BaseController in the PhraseFluent Website. This method retrieves the user's Id from the current JWT (JSON Web Token), ensuring it's available for use across all inheriting controllers.

--- PhraseFluent.Website/Controllers/BaseController.cs
+++ PhraseFluent.Website/Controllers/BaseController.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PhraseFluent.API.Controllers;
 
 public class BaseController : Controller
 {
-    
+    public Guid? UserId
+    {
+        get
+        {
+            var idString = User.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value;
+
+            if (idString == null) return null;
+
+            return Guid.Parse(idString);
+        }
+    }
 }
