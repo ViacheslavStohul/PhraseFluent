c41fa24e1a3b9adc9924b8354d81b240d0674609
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2024-06-08 20:52:58 +0300

    Add 'Completed' and 'TextAnswer' columns to respective tables
    Added a boolean 'Completed' column to the 'TestAttempts' table and a string 'TextAnswer' column to the 'AnswerAttempts' table. These changes are reflected in the data model snapshot. Both are part of the new MySqlTestCompletion migration.

---
+++ PhraseFluent.Website/Migrations/20240608175233_MySqlTestCompletion.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhraseFluent.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MySqlTestCompletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "TestAttempts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TextAnswer",
+                table: "AnswerAttempts",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "TestAttempts");
+
+            migrationBuilder.DropColumn(
+                name: "TextAnswer",
+                table: "AnswerAttempts");
+        }
+    }
+}
