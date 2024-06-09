711879d5cf2eeadb2d3051262a2e16d70b432ba6
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2024-06-09 12:06:21 +0300

    Add "IsCorrect" field to AnswerOption entity
    A new boolean field "IsCorrect" was added to the AnswerOption entity to mark if the option is the correct answer. Corresponding migration and model snapshot have been updated. In addition, getter and setter have been added for the "IsCorrect" field in the AnswerOption class to follow proper convention.

---
+++ PhraseFluent.Website/Migrations/20240609090559_MySqlAnswerOptionsFix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhraseFluent.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MySqlAnswerOptionsFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCorrect",
+                table: "AnswerOptions",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCorrect",
+                table: "AnswerOptions");
+        }
+    }
+}
