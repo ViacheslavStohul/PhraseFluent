f69dedd9fed013925c5656602723e207742ad57f
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2024-12-02 00:18:00 +0200

    Revert "Update interceptor-manager.tsx"
    This reverts commit 041667520c5a1d162d7a0c13cab84298eeaf6542.

--- web/src/components/layouts/interceptor-manager/interceptor-manager.tsx
+++ web/src/components/layouts/interceptor-manager/interceptor-manager.tsx
@@ -23,7 +23,7 @@ const InterceptorManager: FC<AppProps> = (
 
   useEffect(() => {
     axios.interceptors.request.use((config) => {
-      config.baseURL = 'https://192.168.1.106:10192/';
+      config.baseURL = 'http://192.168.1.106:10192/';
       return config;
     });
 
