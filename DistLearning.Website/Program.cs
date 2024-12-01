806f614c97433a611d08ad8498ae567dded18097
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2024-12-01 02:18:19 +0200

    Increase rate limiter permits to 300.
    The rate limiter's permit limit has been increased from 100 to 300 to accommodate higher traffic demands. This change aims to enhance the application's capacity to handle more requests per minute, improving performance under high load conditions.

--- DistLearning.Website/Program.cs
+++ DistLearning.Website/Program.cs
@@ -108,7 +108,7 @@ internal static class Program
                     partitionKey: "global",
                     factory: _ => new FixedWindowRateLimiterOptions
                     {
-                        PermitLimit = 100,
+                        PermitLimit = 300,
                         Window = TimeSpan.FromMinutes(1),
                         QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
                         QueueLimit = 0 
