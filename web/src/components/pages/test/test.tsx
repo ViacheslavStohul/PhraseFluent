63646a1a10e08c1fee0ca65c355a0fe9255d1301
Vlad Astakhov <32707856+Wildrake@users.noreply.github.com>
2024-12-03 23:06:22 +0200

    fix

--- web/src/components/pages/test/test.tsx
+++ web/src/components/pages/test/test.tsx
@@ -150,7 +150,7 @@ const TestPage = () => {
       </div>
       }
       <div className='right'>
-        <button onClick={submit} disabled={test?.card.questionType === 'Text' && (!text || text.length < 2) || (test?.card.questionType !== 'Text' && (!test?.card?.answerOptions || test?.card?.answerOptions.every(option => !option.isCorrect)))}>Наступне питання</button>
+        <button onClick={submit} disabled={(test?.card.questionType !== 'Text' && (!test?.card?.answerOptions || test?.card?.answerOptions.every(option => !option.isCorrect)))}>Наступне питання</button>
       </div>
       </>
       :
