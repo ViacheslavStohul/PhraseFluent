c588138dfdc8b48dcad180015d921a70b35e034f
Vlad Astakhov <32707856+Wildrake@users.noreply.github.com>
2024-12-03 20:49:37 +0200

    fix

--- web/src/components/pages/statistics/question-stats/question-stats.tsx
+++ web/src/components/pages/statistics/question-stats/question-stats.tsx
@@ -13,10 +13,10 @@ const QuestionStats = ({question}: IProps) => {
   return (
     <Card classes='stats-card'>
       <strong>{question.question}</strong>
-      {question.answerOptions &&
+      {question.answerOptions && question.answerOptions.length > 0 &&
         <span>Варіанти відповідей:</span>
       }
-      {question.answerOptions &&
+      {question.answerOptions && question.answerOptions.length > 0&&
         question.answerOptions.map(answer => (
           <div className='answer-block'>
             <span>{answer.optionText} - {answer.selectionCount}{'('+ answer.selectionPercentage+'%)'}</span>
@@ -26,10 +26,10 @@ const QuestionStats = ({question}: IProps) => {
           </div>
         ))
       }
-      {question.textAnswers &&
+      {question.textAnswers && question.textAnswers.length > 0 &&
       <span>Власні відповідей:</span>
       }
-      {question.textAnswers &&
+      {question.textAnswers && question.textAnswers.length > 0 &&
         question.textAnswers.map(answer => (
           <div className='answer-block'>
             <span>{answer.optionText} - {answer.selectionCount}{'('+ answer.selectionPercentage+'%)'}</span>
