badca75478c4e772cf6a371d10a7d5a1c7f96ab0
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2024-12-04 21:09:44 +0200

    Add unit tests for TestsService and normalize answers
    Introduce a test project to house unit tests for the TestsService, focusing on validating answer strings and handling special cases. Implement a NormalizeAnswerString method to trim and properly handle empty answer strings, ensuring consistent processing of user input.

--- DistLearning.Service/TestsService.cs
+++ DistLearning.Service/TestsService.cs
@@ -251,6 +251,7 @@ public class TestsService(ITestRepository testRepository, IMapper mapper) : ITes
                 Text = g.Key!,
                 Count = g.Count()
             })
+            .OrderByDescending(x => x.Count)
             .ToList();
 
         cardDto.TextAnswers = textAnswers;
@@ -278,6 +279,19 @@ public class TestsService(ITestRepository testRepository, IMapper mapper) : ITes
         
         testDto.Cards.Add(cardDto);
     }
+    
+    private static string? NormalizeAnswerString(string? initial)
+    {
+        if (string.IsNullOrEmpty(initial))
+        {
+            return null;
+        }
+        
+        initial = initial.TrimStart();
+        initial = initial.TrimEnd();
+
+        return initial;
+    }
 
     private async Task AddTestAttemptToDb(CardAnswerRequest request, long testAttemptId, Card card)
     {
@@ -300,7 +314,7 @@ public class TestsService(ITestRepository testRepository, IMapper mapper) : ITes
 
                 if (selectedOption.IsAllowedText && !string.IsNullOrEmpty(request.AnswerString))
                 {
-                    answerAttempt.TextAnswer = request.AnswerString;
+                    answerAttempt.TextAnswer = NormalizeAnswerString(request.AnswerString);
                 }
                 testRepository.Add(answerAttempt);
             }
@@ -312,9 +326,9 @@ public class TestsService(ITestRepository testRepository, IMapper mapper) : ITes
                 Uuid = Guid.NewGuid(),
                 TestAttemptId = testAttemptId,
                 CardId = card.Id,
+                TextAnswer = NormalizeAnswerString(request.AnswerString),
                 TestAttempt = null,
                 Card = null,
-                TextAnswer = request.AnswerString
             };
 
             testRepository.Add(answerAttempt);
