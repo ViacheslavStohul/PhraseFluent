cd514e55e0f9f9e78fc40cd7ec1dc8efbd2303b4
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2024-12-04 22:48:05 +0200

    Normalize text answers during data initialization
    Add a private method to trim leading and trailing spaces from text answers if they are found unprocessed during data context initialization. This ensures that text answers are stored consistently without leading or trailing whitespace. Commit also includes the necessary migration check to avoid redundant operations.

--- DistLearning.DataAccess/DataContext.Initialize.cs
+++ DistLearning.DataAccess/DataContext.Initialize.cs
@@ -2,6 +2,8 @@
 
 namespace DistLearning.DataAccess;
 
+using Microsoft.EntityFrameworkCore;
+
 public partial class DataContext 
 {
     public async Task Initialize()
@@ -9,7 +11,10 @@ public partial class DataContext
         await using var transaction = await Database.BeginTransactionAsync();
         try
         {
-            var transactions = CompleteInitializers.Select(x => x.CompleteInitializerId);
+            var completedMigrations = await CompleteInitializers.Select(x => x.CompleteInitializerId).ToListAsync();
+
+            await RenderTextAnswers(completedMigrations);
+            
             await transaction.CommitAsync();
         }
         catch
@@ -17,5 +22,24 @@ public partial class DataContext
             await transaction.RollbackAsync();
             throw;
         }
+    }
+
+    private async Task RenderTextAnswers(List<Guid> completeMigrations)
+    {
+        var migrationId = Guid.Parse("A0A9CA66-181D-471C-8F37-A818AD924904");
+        
+        if (completeMigrations.Contains(migrationId)) return;
+
+        var unprocessedTextAnswers = AnswerAttempts.Where(x => x.TextAnswer != null && (x.TextAnswer.EndsWith(' ') || x.TextAnswer.StartsWith(' '))).ToList();
+
+        foreach (var answer in unprocessedTextAnswers)
+        {
+            answer.TextAnswer = answer.TextAnswer!.TrimStart(' ');
+            answer.TextAnswer = answer.TextAnswer!.TrimEnd(' ');
+        }
+        
+        completeMigrations.Add(migrationId);
+
+        await SaveChangesAsync();
     }
 }
