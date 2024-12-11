0a767fccdc99d8044c26b32b18c2efd1e2aec8c7
Vlad Astakhov <32707856+Wildrake@users.noreply.github.com>
2024-12-12 00:03:45 +0200

    fix

--- web/src/components/pages/edit/create-question-card/create-question-card.tsx
+++ web/src/components/pages/edit/create-question-card/create-question-card.tsx
@@ -102,13 +102,13 @@ const CreateQuestionCard = ({emit, testId}: IProps) => {
           onChange={(value) => handleChange('questionType',value?.value??'')}/>
         </div>
       { card.questionType === 'TestOneAnswer' || card.questionType === 'TestManyAnswers' &&
-        <>
           <div className='answer-grid'>
             { card.answerOptions && card.answerOptions.map((option, index)=> (
                 <AnswerCard option={option} onChange={(value)=> changeOption(value, index)} onDelete={()=> deleteOption(index)} key={index}/>))}
           </div>
+      }
+      { card.questionType === 'TestOneAnswer' || card.questionType === 'TestManyAnswers' &&
           <Checkbox label='Додати власну відповідь' checked={custom} onChange={() => setCustom(!custom)}/>
-        </>
       }
       <div className='buttons'>
         { (card.questionType === 'TestOneAnswer' || card.questionType === 'TestManyAnswers') &&
